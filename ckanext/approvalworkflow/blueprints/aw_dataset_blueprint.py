0bb532ec762b6ff19df59267cc1abdf76a5145d0
blagoja <blagojabozinovski@gmail.com>
2025-11-07 10:43:49 +0100

    Comment EditView class

--- ckanext/approvalworkflow/blueprints/aw_dataset_blueprint.py
+++ ckanext/approvalworkflow/blueprints/aw_dataset_blueprint.py
@@ -37,149 +37,149 @@ dataset_approval_workflow = Blueprint(
 )
 
 
-class ApprovalEditView(MethodView):
-    def _prepare(self, id, data=None):
-        context = {
-            u'model': model,
-            u'session': model.Session,
-            u'user': g.user,
-            u'auth_user_obj': g.userobj,
-            u'save': u'save' in request.form
-        }
-        return context
+# class ApprovalEditView(MethodView):
+#     def _prepare(self, id, data=None):
+#         context = {
+#             u'model': model,
+#             u'session': model.Session,
+#             u'user': g.user,
+#             u'auth_user_obj': g.userobj,
+#             u'save': u'save' in request.form
+#         }
+#         return context
 
-    def post(self, package_type, id):
-        context = self._prepare(id)
-        package_type = _get_package_type(id) or package_type
-        log.debug(u'Package save request name: %s POST: %r', id, request.form)
-        try:
-            data_dict = clean_dict(
-                dict_fns.unflatten(tuplize_dict(parse_params(request.form)))
-            )
-        except dict_fns.DataError:
-            return base.abort(400, _(u'Integrity Error'))
-        try:
-            if u'_ckan_phase' in data_dict:
-                # we allow partial updates to not destroy existing resources
-                context[u'allow_partial_update'] = True
-                if u'tag_string' in data_dict:
-                    data_dict[u'tags'] = dataset._tag_string_to_list(
-                        data_dict[u'tag_string']
-                    )
-                del data_dict[u'_ckan_phase']
-                del data_dict[u'save']
-            context[u'message'] = data_dict.get(u'log_message', u'')
-            data_dict['id'] = id
-            data_dict['state'] = 'active'
-            pkg_dict = get_action(u'package_update')(context, data_dict)
+#     def post(self, package_type, id):
+#         context = self._prepare(id)
+#         package_type = _get_package_type(id) or package_type
+#         log.debug(u'Package save request name: %s POST: %r', id, request.form)
+#         try:
+#             data_dict = clean_dict(
+#                 dict_fns.unflatten(tuplize_dict(parse_params(request.form)))
+#             )
+#         except dict_fns.DataError:
+#             return base.abort(400, _(u'Integrity Error'))
+#         try:
+#             if u'_ckan_phase' in data_dict:
+#                 # we allow partial updates to not destroy existing resources
+#                 context[u'allow_partial_update'] = True
+#                 if u'tag_string' in data_dict:
+#                     data_dict[u'tags'] = dataset._tag_string_to_list(
+#                         data_dict[u'tag_string']
+#                     )
+#                 del data_dict[u'_ckan_phase']
+#                 del data_dict[u'save']
+#             context[u'message'] = data_dict.get(u'log_message', u'')
+#             data_dict['id'] = id
+#             data_dict['state'] = 'active'
+#             pkg_dict = get_action(u'package_update')(context, data_dict)
 
-            return dataset._form_save_redirect(
-                pkg_dict[u'name'], u'edit', package_type=package_type
-            )
-        except NotAuthorized:
-            return base.abort(403, _(u'Unauthorized to read package %s') % id)
-        except NotFound:
-            return base.abort(404, _(u'Dataset not found'))
-        except SearchIndexError as e:
-            try:
-                exc_str = text_type(repr(e.args))
-            except Exception:  # We don't like bare excepts
-                exc_str = text_type(str(e))
-            return base.abort(
-                500,
-                _(u'Unable to update search index.') + exc_str
-            )
-        except ValidationError as e:
-            errors = e.error_dict
-            error_summary = e.error_summary
-            return self.get(package_type, id, data_dict, errors, error_summary)
+#             return dataset._form_save_redirect(
+#                 pkg_dict[u'name'], u'edit', package_type=package_type
+#             )
+#         except NotAuthorized:
+#             return base.abort(403, _(u'Unauthorized to read package %s') % id)
+#         except NotFound:
+#             return base.abort(404, _(u'Dataset not found'))
+#         except SearchIndexError as e:
+#             try:
+#                 exc_str = text_type(repr(e.args))
+#             except Exception:  # We don't like bare excepts
+#                 exc_str = text_type(str(e))
+#             return base.abort(
+#                 500,
+#                 _(u'Unable to update search index.') + exc_str
+#             )
+#         except ValidationError as e:
+#             errors = e.error_dict
+#             error_summary = e.error_summary
+#             return self.get(package_type, id, data_dict, errors, error_summary)
 
-    def get(
-        self, package_type, id, data=None, errors=None, error_summary=None
-    ):
-        context = self._prepare(id, data)
-        package_type = _get_package_type(id) or package_type
-        try:
-            pkg_dict = get_action(u'package_show')(
-                dict(context, for_view=True), {
-                    u'id': id
-                }
-            )
-            context[u'for_edit'] = True
-            old_data = get_action(u'package_show')(context, {u'id': id})
+#     def get(
+#         self, package_type, id, data=None, errors=None, error_summary=None
+#     ):
+#         context = self._prepare(id, data)
+#         package_type = _get_package_type(id) or package_type
+#         try:
+#             pkg_dict = get_action(u'package_show')(
+#                 dict(context, for_view=True), {
+#                     u'id': id
+#                 }
+#             )
+#             context[u'for_edit'] = True
+#             old_data = get_action(u'package_show')(context, {u'id': id})
 
-            if data:
-                old_data.update(data)
-            data = old_data
-        except (NotFound, NotAuthorized):
-            return base.abort(404, _(u'Dataset not found'))
+#             if data:
+#                 old_data.update(data)
+#             data = old_data
+#         except (NotFound, NotAuthorized):
+#             return base.abort(404, _(u'Dataset not found'))
 
-        if data.get(u'state', u'').startswith(u'draft'):
-            g.form_action = h.url_for(u'{}.new'.format(package_type))
-            g.form_style = u'new'
+#         if data.get(u'state', u'').startswith(u'draft'):
+#             g.form_action = h.url_for(u'{}.new'.format(package_type))
+#             g.form_style = u'new'
 
-            return dataset.CreateView().get(
-                package_type,
-                data=data,
-                errors=errors,
-                error_summary=error_summary
-            )
+#             return dataset.CreateView().get(
+#                 package_type,
+#                 data=data,
+#                 errors=errors,
+#                 error_summary=error_summary
+#             )
 
-        pkg = context.get(u"package")
-        resources_json = h.json.dumps(data.get(u'resources', []))
+#         pkg = context.get(u"package")
+#         resources_json = h.json.dumps(data.get(u'resources', []))
 
-        try:
-            logic.check_access(u'package_update', context, {u'id': id})
-        except NotAuthorized:
-            return base.abort(
-                403,
-                _(u'User %r not authorized to edit %s') % (g.user, id)
-            )
+#         try:
+#             logic.check_access(u'package_update', context, {u'id': id})
+#         except NotAuthorized:
+#             return base.abort(
+#                 403,
+#                 _(u'User %r not authorized to edit %s') % (g.user, id)
+#             )
 
-        if data and not data.get(u'tag_string'):
-            data[u'tag_string'] = u', '.join(
-                h.dict_list_reduce(pkg_dict.get(u'tags', {}), u'name')
-            )
-        errors = errors or {}
-        form_snippet = _get_pkg_template(
-            u'package_form', package_type=package_type
-        )
-        form_vars = {
-            u'data': data,
-            u'errors': errors,
-            u'error_summary': error_summary,
-            u'action': u'edit',
-            u'dataset_type': package_type,
-            u'form_style': u'edit'
-        }
-        errors_json = h.json.dumps(errors)
+#         if data and not data.get(u'tag_string'):
+#             data[u'tag_string'] = u', '.join(
+#                 h.dict_list_reduce(pkg_dict.get(u'tags', {}), u'name')
+#             )
+#         errors = errors or {}
+#         form_snippet = _get_pkg_template(
+#             u'package_form', package_type=package_type
+#         )
+#         form_vars = {
+#             u'data': data,
+#             u'errors': errors,
+#             u'error_summary': error_summary,
+#             u'action': u'edit',
+#             u'dataset_type': package_type,
+#             u'form_style': u'edit'
+#         }
+#         errors_json = h.json.dumps(errors)
 
-        g.pkg = pkg
-        g.resources_json = resources_json
-        g.errors_json = errors_json
+#         g.pkg = pkg
+#         g.resources_json = resources_json
+#         g.errors_json = errors_json
 
-        _setup_template_variables(
-            context, {u'id': id}, package_type=package_type
-        )
+#         _setup_template_variables(
+#             context, {u'id': id}, package_type=package_type
+#         )
 
-        form_vars[u'stage'] = [u'active']
-        if data.get(u'state', u'').startswith(u'draft'):
-            form_vars[u'stage'] = [u'active', u'complete']
+#         form_vars[u'stage'] = [u'active']
+#         if data.get(u'state', u'').startswith(u'draft'):
+#             form_vars[u'stage'] = [u'active', u'complete']
 
-        edit_template = _get_pkg_template(u'edit_template', package_type)
-        return base.render(
-            edit_template,
-            extra_vars={
-                u'form_vars': form_vars,
-                u'form_snippet': form_snippet,
-                u'dataset_type': package_type,
-                u'pkg_dict': pkg_dict,
-                u'pkg': pkg,
-                u'resources_json': resources_json,
-                u'form_snippet': form_snippet,
-                u'errors_json': errors_json
-            }
-        )
+#         edit_template = _get_pkg_template(u'edit_template', package_type)
+#         return base.render(
+#             edit_template,
+#             extra_vars={
+#                 u'form_vars': form_vars,
+#                 u'form_snippet': form_snippet,
+#                 u'dataset_type': package_type,
+#                 u'pkg_dict': pkg_dict,
+#                 u'pkg': pkg,
+#                 u'resources_json': resources_json,
+#                 u'form_snippet': form_snippet,
+#                 u'errors_json': errors_json
+#             }
+#         )
 
 
 class ApprovalWorkflowRejectView(MethodView):
@@ -236,9 +236,9 @@ class ApprovalWorkflowRejectView(MethodView):
         )
 
 
-dataset_approval_workflow.add_url_rule(
-    u'/edit/<id>', view_func=ApprovalEditView.as_view(str(u'edit'))
-)
+# dataset_approval_workflow.add_url_rule(
+#     u'/edit/<id>', view_func=ApprovalEditView.as_view(str(u'edit'))
+# )
 dataset_approval_workflow.add_url_rule(
     u'/reject/<id>', view_func=ApprovalWorkflowRejectView.as_view(str(u'reject'))
 )
